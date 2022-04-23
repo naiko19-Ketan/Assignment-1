72b273723b8bd5aca0fefa645be1a58ed06cd85c
Ketan <103490646+naiko19-Ketan@users.noreply.github.com>
2022-04-23 20:34:03 +0530

    Delete Q2 output.py

--- Q2 output.py
+++
@@ -1,8 +0,0 @@
-Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-========== RESTART: C:/Users/Rupesh/Desktop/ITC assignment/Q2 input.py =========
-Enter your gross income: 25000
-Enter the no. of dependents: 2
-Your annual income tax is 1800.0
->>> 
