a40269ae657f4ab466f68cb02dda2dd0aafd55ad
Ketan <103490646+naiko19-Ketan@users.noreply.github.com>
2022-04-26 14:05:22 +0530

    Update Q2 input.py

--- Q2 input.py
+++ Q2 input.py
@@ -1,7 +1,7 @@
-total_income=int(input())
-no_of_dependents=int(input())
+total_income=int(input('Enter your Gross income: '))
+no_of_dependents=int(input('Enter number of dependents: '))
 standard_deduction=10000
 dependent_deduction=3000
 taxable_income= total_income - standard_deduction - (dependent_deduction*(no_of_dependents))
 tax=(taxable_income*20)/100
-print(tax)
+print('Your annual income tax is',tax)
