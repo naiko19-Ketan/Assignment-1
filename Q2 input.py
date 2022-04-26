3b9cb7ac5d1526b8050bca87db396a04a411ff93
Ketan <103490646+naiko19-Ketan@users.noreply.github.com>
2022-04-26 14:02:51 +0530

    Update Q2 input.py

--- Q2 input.py
+++ Q2 input.py
@@ -1,6 +1,7 @@
 total_income=int(input())
 no_of_dependents=int(input())
 standard_deduction=10000
-taxable_income= total_income - 10000 - (3000*(no_of_dependents))
+dependent_deduction=3000
+taxable_income= total_income - standard_deduction - (dependent_deduction*(no_of_dependents))
 tax=(taxable_income*20)/100
 print(tax)
