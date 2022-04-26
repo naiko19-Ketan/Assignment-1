49ec3bce898a3878aa766110003cba7650110077
Ketan <103490646+naiko19-Ketan@users.noreply.github.com>
2022-04-26 13:51:16 +0530

    Update Q2 input.py

--- Q2 input.py
+++ Q2 input.py
@@ -1,7 +1,6 @@
-SID=int((input('Enter your SID: ')))
-Name=str((input('Enter your Name: ')))
-Gender=str((input('Enter your gender(M/F/U): ')))
-Course=str((input('Enter your Course name: ')))
-CGPA=float(input('Enter your CGPA: '))
-student=[SID,Name,Gender,Course,CGPA]
-print(student)
+total_income=int(input())
+no_of_dependents=int(input())
+standard_deduction=10000
+taxable_income= total_income - 10000 - (3000*(no_of_dependents))
+tax=(taxable_income*20)/100
+print(tax)
